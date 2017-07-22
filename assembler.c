c82074392852ce3592c75d01be8880202ccdf367
Barak Kinarti <barak.kinarti@sap.com>
2017-07-22 22:22:55 +0300

    calculate data size

--- assembler.c
+++ assembler.c
@@ -1,5 +1,7 @@
+#include <string.h>
 #include "file_content.h"
 #include "assembler.h"
+#include "logger.h"
 
 void firstIteration() {
     int i, commandSize;
@@ -9,8 +11,9 @@ void firstIteration() {
     for (i=0; i < fileContent->noOfLines; i++) { /* For every line in file */
         FileLine line = fileContent->lines[i];
         int hasLabel = 0;
+        int dataSize = 0;
 
-        if(line.label) {
+        if(strlen(line.label) > 0) {
             hasLabel = 1;
         }
 
@@ -19,6 +22,49 @@ void firstIteration() {
         /* handle external */
 
 
-        /* handle storage */
+        /* handle storage entities */
+        if (equals(line.command, DATA) || equals(line.command, MAT)) {
+            dataSize = getCommaCount(line.lineContent) + 1;
+//            LOG_TRACE(LOG_DEBUG,"encountered .data line: %s, dataSize: %d\n", line.lineContent, dataSize);
+            /*TODO: push to data table*/
+        } else if(equals(line.command, STRING)) {
+            trimStringQuotes(line.op1); /* remove string quotes*/
+            dataSize = strlen(line.op1);
+//            LOG_TRACE(LOG_DEBUG,"encountered .string line: %s, dataSize: %d\n", line.lineContent, dataSize);
+            /*TODO: push to data table*/
+        }
+        dc += dataSize;
     }
+    LOG_TRACE(LOG_DEBUG,"Final Data Size: %d\n",dc);
+
 };
+
+int getCommaCount(char *str) {
+    int i,
+            counter = 0,
+            len = strlen(str);
+
+    for (i = 0; i < len; i++) {
+        if (str[i] == ',') {
+            counter++;
+        }
+    }
+    return counter;
+}
+
+char *trimStringQuotes(char *str) {
+    int i, j = 0, len = strlen(str);
+    for (i = 0; i < len; i ++) {
+        if (str[i] != '"' && str[i] != '\\') {
+            str[j++] = str[i];
+        } else if (str[i+1] == '"' && str[i] == '\\') {
+            str[j++] = '"';
+        } else if (str[i+1] != '"' && str[i] == '\\') {
+            str[j++] = '\\';
+        }
+    }
+
+//You missed the string termination ;)
+    if(j>0) str[j]=0;
+    return str;
+}
--- assembler.h
+++ assembler.h
@@ -1,5 +1,13 @@
 #define MEMORY_STARTING_ADDRESS 100
+#define DATA ".data"
+#define MAT ".mat"
+#define STRING ".string"
+#define equals(str1, str2) strcmp(str1, str2) == 0
 
 void firstIteration();
+int getCommaCount(char*);
+char *trimStringQuotes(char *);
+
+
 int ic;
 int dc;
